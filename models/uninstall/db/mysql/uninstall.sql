22b42be5ffa551c476031154657411c566bf9f13
xpointer <xpointer@e4632201-f7d7-4473-b05d-be2f3bff536f>
2013-02-15 16:25:30 +0000

    Also delete Posts CJT Blocks Metaboxes data from postmeta table.
    git-svn-id: svn://localhost/_tmp/css-javascript-toolbox-svn/trunk@179 e4632201-f7d7-4473-b05d-be2f3bff536f

--- models/uninstall/db/mysql/uninstall.sql
+++ models/uninstall/db/mysql/uninstall.sql
@@ -27,4 +27,8 @@ DELETE FROM #__wordpress_options where option_name = 'meta-box-order_cjtoolbox';
 DELETE FROM #__wordpress_usermeta where meta_key = 'closedpostboxes_cjtoolbox';
 
 /* User Settings */
-DELETE FROM #__wordpress_options where option_name = 'cjt-settings.CJTSettingsMetaboxPage'
+DELETE FROM #__wordpress_options where option_name = 'cjt-settings.CJTSettingsMetaboxPage';
+
+/*  Posts Metabox blocks */
+DELETE FROM #__wordpress_postmeta where meta_key = '__CJT-BLOCK-ID';
+DELETE FROM #__wordpress_postmeta where meta_key = '__CJT-BLOCK-STATUS'
