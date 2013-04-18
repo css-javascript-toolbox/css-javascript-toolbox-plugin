e81175006467e307fc11a21378a594b2011d13c0
Ahmed Said Hamed <xpointer@hotmail.com>
2013-04-18 21:35:58 +0200

    Fix: Issue #3.

--- models/extensions.php
+++ models/extensions.php
@@ -17,7 +17,7 @@ class CJTExtensionsModel {
 	*/
 	public function getListTypeName() {
 		// if the search term is for CJT extensions return 'extensions' otherwise return 'plugins'
-		$type = (isset($_REQUEST['s']) == CJTExtensionsAccessPoint::PLUGINS_PAGE_SEARCH_TERM) ? 'extensions' : 'plugins';
+		$type = (isset($_REQUEST['s']) && ($_REQUEST['s'] == CJTExtensionsAccessPoint::PLUGINS_PAGE_SEARCH_TERM)) ? 'extensions' : 'plugins';
 		return $type;
 	}
 	
