512f3638bc4b956fdacfc6ae8dfab67cd780439b
Ahmed Said Hamed <xpointer@hotmail.com>
2013-05-28 18:41:59 +0200

    - Maximum differed Shortcode framework, so no Shortcode code will be loaded until cjtoolbox shortcodeis being included in the request. - As long as the block state is active, block can be queried using any field (e.g id, name, owner, created date, etc...). - Shortcodes is only supported for block however the framework is much flixible to accept other types (e.g template).

--- controllers/blocks-coupling.php
+++ controllers/blocks-coupling.php
@@ -48,6 +48,13 @@ class CJTBlocksCouplingController extends CJTController {
 	* 
 	* @var mixed
 	*/
+	protected static $instance = null;
+	
+	/**
+	* put your comment there...
+	* 
+	* @var mixed
+	*/
 	private $onActionIds = array();
 	
 	/**
@@ -162,6 +169,12 @@ class CJTBlocksCouplingController extends CJTController {
 	* @return void
 	*/
 	public function __construct() {
+		// Only one instance is allowed.
+		if (self::$instance) {
+			throw new Exception('Trying to instantiate multiple coupling instances!!');
+		}
+		// Hold the single instance we've!
+		self::$instance = $this;
 		// Initialize controller.
 		parent::__construct(false);
 		// Import related libraries
@@ -176,6 +189,15 @@ class CJTBlocksCouplingController extends CJTController {
 		add_shortcode('cjtoolbox', array(&$this, 'shortcode'));
 	}
 	
+	/**
+	* put your comment there...
+	* 
+	* @param mixed $id
+	*/
+	public function addOnActionIds($id) {
+		$this->onActionIds[] = $id;
+	}
+
 	/**
 	* put your comment there...
 	* 
@@ -247,7 +269,6 @@ class CJTBlocksCouplingController extends CJTController {
 					}
 				}
 				// For every location store blocks code into single string
-				/** @todo  Use method other data:// wrapper, its only available in Hight version of PHP (5.3 or so!) */
 				$evaluatedCode = CJTPHPCodeEvaluator::getInstance($block)->exec()->getOutput();
 				/** @todo Include Debuging info only if we're in debuging mode! */
 				if (1) {
@@ -255,7 +276,7 @@ class CJTBlocksCouplingController extends CJTController {
 				}
 				$this->blocks['code'][$block->location] .= $this->onappendcode($evaluatedCode);
 				// Store all used Ids in the CORRECT ORDER.
-				$this->onActionIds[] = $blockId;
+				$this->addOnActionIds($blockId);
 			}
 		}
 		// Return true if there is at least 1 block return within the set.
@@ -278,7 +299,15 @@ class CJTBlocksCouplingController extends CJTController {
 	public function getFilters() {
 		return $this->ongetfilters($this->filters);	
 	}
-	
+
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function getOnActionIds() {
+		return $this->onActionIds;
+	}
+
 	/**
 	* put your comment there...
 	* 
@@ -294,6 +323,14 @@ class CJTBlocksCouplingController extends CJTController {
 		// Final URL.
 		$url = "{$protocol}{$host}{$port}{$requestURI}";
 		return $url;
+	}
+	
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function hasOnActionIds() {
+		return !empty($this->onActionIds);
 	}
 	
 	/**
@@ -446,52 +483,31 @@ class CJTBlocksCouplingController extends CJTController {
 	}
 	
 	/**
-	* put your comment there...
+	* Wordpress do shortcode callback for
+	* CJT Shortcodes ([cjtoolbox ....])! 
+	* 
+	* This method role is to load the shortcode routines
+	* in order to handle the request.
+	* 
+	* It doesn't do anything except deferring the shortcode
+	* codes from loaded until shortcode is really used!
 	* 
 	* @param mixed $attributes
 	*/
 	public function shortcode($attributes) {
-		// Initialize vars.
-		$replacement = '';
-		// Default Class.
-		if (!isset($attributes['class'])) {
-			$class =	'block';
-		}
-		switch ($class) {
-			case 'block':
-				// Get is the default "operation"!
-				if (!isset($attributes['op'])) {
-					$attributes['op'] = 'get';
-				}
-				switch ($attributes['op']) {
-					case 'get': 
-						// Import dependecies.
-						cssJSToolbox::import('framework:db:mysql:xtable.inc.php', 'framework:php:evaluator:evaluator.inc.php');
-						// Output block if 'force="true" or only if it wasn't already in the header/footer!
-						if ((((isset($attributes['force'])) && ($attributes['force'] == "true")) || !in_array($attributes['id'], $this->onActionIds))) {
-							// Id is being used!
-							if ((isset($attributes['force'])) && ($attributes['force'] != 'true')) {
-								$this->onActionIds[] = (int) $attributes['id'];
-							}
-							// Get block code.
-							$block = CJTxTable::getInstance('block')
-																						->set('id', $attributes['id'])
-																						->load();
-							// Only ACTIVE blocks!
-							if ($block->get('state') != 'active') {
-								return;
-							}
-							// Get block code, execute it as PHP!
-							$replacement = CJTPHPCodeEvaluator::getInstance($block->getData())->exec()->getOutput();
-						}
-					break;
-				}
-			break;
-			default:
-				$replacement = cssJSToolbox::getText('Shortcode Type is not supported!! Only (block) type is currently available!!!');
-			break;
-		}
-		return $replacement;
+		// Instantiate Shortcode handler class.
+		cssJSToolbox::import('controllers:coupling:shortcode:shortcode.php');
+		$shortcode = new CJT_Controllers_Coupling_Shortcode($attributes);
+		// Return Shortcode replacement!
+		return ((string) $shortcode);
+	}
+	
+	/**
+	* put your comment there...
+	* 
+	*/
+	public static function & theInstance() {
+		return self::$instance;
 	}
 	
 } // End class.
