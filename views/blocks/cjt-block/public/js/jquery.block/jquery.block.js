fe2d5da9d74ed50729e5e00e33a5692d1fb60d32
A.S <xpointer@hotmail.com>
2013-08-12 01:05:38 +0200

    = The commit is only for getting client feedback =
    - Update PHP code for displaying the new HTML code for displaying the assignment panel.
    - Use for custom posts instead of TABS, make the accordion container scrollable so if the custom posts is overflowed, can be reached.

--- views/blocks/cjt-block/public/js/jquery.block/jquery.block.js
+++ views/blocks/cjt-block/public/js/jquery.block/jquery.block.js
@@ -18,44 +18,81 @@
 		/**
 		* 
 		*/
-		this._onadvancedaccordionchanged = function(event, ui) {
-			// Activate textarea under the current selected item content!
-			ui.newContent.find('textarea').focus();
-		}
+		this.pagesPanel = null;
 		
 		/**
-		* 
-		*/
-		this._onselectchilds = function(event) {
-			// Initialize vars.
-			var overlay = $(event.target);
-			var checkbox = overlay.parent().find('.select-childs');
-			var state = checkbox.prop('checked') ? '' : 'checked';
-			// Work only if select-child checkbox is interactive!
-			if (checkbox.attr('disabled') != 'disabled') {
-				// Revert checkbox state.
-				checkbox.prop('checked', state);
-				// Clone state to parent checkbox.
-				checkbox.parent().find('label>input:checkbox').prop('checked', state).trigger('change');
-				//Clone state to all child checkboxes
-				checkbox.parent().find('.children input:checkbox').prop('checked', state).trigger('change');
+		*
+		*
+		*
+		*
+		*/		
+		this._ontogglepagespanel = function(event, initialState) {
+			var toggler = $(event.target);
+			var block = this.block;
+			var tabs = block.box.find('.cjt-pages-tab');
+			var pagesBlock = block.box.find('.cjpageblock');
+			var codeBlock = block.box.find('.cjcodeblock');
+			var aceEditor = block.aceEditor;
+			var newState = '';
+			// Hide pages panel when:
+			if (tabs.css('display') != 'none') {
+				// Hide if initial value == undefined or initial value == closed.
+				if ((initialState != '') && (initialState != 'undefined')) {
+					// Hide elements.
+					tabs.hide();
+					pagesBlock.css('width', '0px');
+					codeBlock.animate({'margin-right' : 0}, undefined, undefined, 
+						function() {
+							toggler.addClass('closed');
+							// Refresh editor.
+							aceEditor.resize();
+						}
+					);
+					// Save state.
+					newState = 'closed';
+				}
 			}
+			else {
+				// Show elements.
+				codeBlock.animate({'margin-right' : 320}, undefined, undefined,
+					function() {
+						// Use CSS class margin not inline style!
+						codeBlock.css('margin-right', '');
+						// Show panel!
+						pagesBlock.css('width', '');
+						tabs.show();
+						toggler.removeClass('closed');
+						// Refresh editor.
+						aceEditor.resize();
+					}
+				);
+			}
+			// Set title based on the new STATE!
+			toggler.attr('title', CJT_CJT_BLOCKJqueryBlockI18N['assigmentPanel_' + newState + 'Title']);
+			// Save state.
+			block.set('pagesPanelToggleState', newState);
 			// For link to behave inactive.
 			return false;
 		}
-		
 		// Initialize parent class.
 		this.initCJTPluginBase(node, args);
-		// Activate objects panel!
-		var pagesPanel = this.block.box.find('.cjt-pages-tab').tabs();
-		// Accordion menu for Advanced TAB.
-		this.block.box.find('#advanced-accordion-' + this.block.get('id')).accordion({
-				change : this._onadvancedaccordionchanged,
-				header: '.acc-header'
-			}
-		);
-		// Put select-childs checkboxes in action!
-		pagesPanel.find('.select-childs-checkbox-overlay').click($.proxy(this._onselectchilds, this));
+		// Plug the assigment panel, get the jQuery ELement for it
+		var assigmentPanelElement = this.block.box.find('#tabs-' + this.block.get('id'));
+		this.pagesPanel = assigmentPanelElement.CJTBlockAssignmentPanel({block : this});
+		// Add toolbox button.
+		var tbIconsGroup = this.block.box.find('.editor-toolbox .icons-group')
+		tbIconsGroup.children().first().after('<a href="#" class="cjt-tb-link cjttbl-toggle-objects-panel"></a>')
+		var toggler = this.editorToolbox.add('toggle-objects-panel', {callback : this._ontogglepagespanel});
+		// Close it if it were closed.
+		this._ontogglepagespanel({target : toggler.jButton}, this.block.get('pagesPanelToggleState', ''));
+		
+		// More to Dock with Fullscreen mode!
+		this.extraDocks = [
+			{element : assigmentPanelElement.find('.ui-tabs-panel'), pixels : 89},
+			{element : assigmentPanelElement.find('.ui-tabs-panel .pagelist'), pixels : 132},
+			{element : assigmentPanelElement.find('.advanced-accordion .ui-accordion-content'), pixels : 169},
+			{element : assigmentPanelElement.find('.advanced-accordion .ui-accordion-content textarea'), pixels : 177}
+		];
 	} // End class.
 	
 	// Extend CJTBLockPluginBase.
