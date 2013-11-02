0f661bbfe889a7be554a491eeb865eb274f84307
AHMeD <xpointer@hotmail.com>
2013-11-02 15:35:28 +0200

    Load unloaded block codes only when time as it loaded the code every time the code block is closed and opened.

--- views/blocks/block/public/js/jquery.block/jquery.block.js
+++ views/blocks/block/public/js/jquery.block/jquery.block.js
@@ -538,7 +538,7 @@
 						// Remove loading state.
 						this.block.aceEditor.setReadOnly(false);
 						// Mark as loaded.
-						jEditor.removeClass('iniially-loaded');
+						jEditor.addClass('initially-loaded');
 					}, this)
 				);
 			}
