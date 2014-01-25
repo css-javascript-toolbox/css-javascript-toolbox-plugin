ed8f703d26d5a742839ba2e608fd4c28c2c6f00b
AHMeD <xpointer@hotmail.com>
2014-01-25 22:18:40 +0200

    CSS-FIXES is now compatible with WP < 3.8 as well. Also block-name and edit-block-name font size is getting back to 15px as before.

---
+++ framework/wordpress/Currentversion.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* 
+*/
+
+/**
+* 
+*/
+class CJT_Framework_Wordpress_Currentversion {
+	
+	/**
+	* put your comment there...
+	* 
+	* @var mixed
+	*/
+	protected $version;
+	
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function __construct() {
+		// Read current version.
+		$this->version = get_bloginfo('version');
+	}
+
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function getVersion() {
+		return $this->version;
+	}
+
+	/**
+	* put your comment there...
+	* 
+	* @param mixed $version
+	*/
+	public function isLess($version) {
+		return (version_compare('3.6', $version) == -1);
+	}
+
+} // End class.
